f8b6efc0b8257f3b2e040f681df09ae0a457b51d
Manolo Vera <manolo.vn@gmail.com>
2016-03-30 14:48:42 +0200

    code style

--- rxsensor/src/main/java/com/getwandup/rxsensor/transformer/LowPassFilter.java
+++ rxsensor/src/main/java/com/getwandup/rxsensor/transformer/LowPassFilter.java
@@ -32,7 +32,9 @@ public class LowPassFilter implements Transformer<RxSensorEvent, RxSensorEvent> 
     }
 
     protected float[] lowPass(float[] input, float[] output) {
-        if (output == null) return input;
+        if (output == null) {
+            return input;
+        }
         for (int i = 0; i < input.length; i++) {
             output[i] = output[i] + factor * (input[i] - output[i]);
         }
